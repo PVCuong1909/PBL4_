aafa9d0c748c882f6c0358ee8c3534ed0efe45e7
mrcuongcom <mrcuongcom3@gmail.com>
2024-01-03 21:59:08 +0700

    commit all

---
+++ FE/FE_Web/src/views/home/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Header from "../../organisms/Header.vue";
+import SearchComponent from "../mainjob/search/index.vue";
+import { useAuthStore } from "@/stores/auth";
+defineProps<{}>();
+const auth = useAuthStore();
+</script>
+
+
+<template>
+  <Header />
+  <div class="__default__ body-layout">
+    <!-- <SearchComponent /> -->
+    <slot class="body">
+      
+    </slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.body-layout {
+  margin: 0 auto;
+  width: 1300px;
+  max-width: 100%;
+  margin-top: 70px;
+}
+.bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
+}
+</style>
---
+++ FE/FE_Web/src/views/mainjob/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <SearchComponent />
+    <!-- <JobList /> -->
+  </div>
+</template>
+<script setup lang="ts">
+import SearchComponent from "../mainjob/search/index.vue";
+import JobList from "../mainjob/joblist/index.vue";
+</script>
+<style scoped></style>
